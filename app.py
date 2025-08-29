2525a5f8baad2c374ea3484b33431c79ff537479
Mehwish <mehwish.naseer39@gmail.com>
2025-08-30 00:13:22 +0100

    Add my-picture.png to static/images

--- app.py
+++ app.py
@@ -36,11 +36,7 @@ def contact() -> str:
     return render_template('contact.html')
 
 
-# if __name__ == '__main__':
-#     # For local development enable debug mode for hot reloading.
-#     app.run(debug=True)
-
 if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=True)
